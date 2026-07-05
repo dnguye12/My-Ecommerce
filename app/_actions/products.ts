bfb11637c07d60bd1a127016c08434a1033565a5
Duc Huy Nguyen <huy.nguyen@coderpad.io>
2026-07-05 09:40:17 +0200

    feat: products queries

---
+++ app/_actions/products.ts
@@ -0,0 +1,29 @@
+'use server'
+
+import { db } from '@/db'
+import { products } from '@/db/schema'
+import { eq } from 'drizzle-orm'
+import { revalidatePath } from 'next/cache'
+import * as z from 'zod'
+
+const productSchema = z.object({
+  name: z.string().min(1, 'Required'),
+  description: z.string().min(1, 'Required'),
+  price: z.string().min(1, 'Required'),
+  imageUrl: z.string().url('Upload an image'),
+  category: z.string().min(1, 'Required'),
+  stock: z.coerce.number<number>().int().min(0)
+})
+
+export const createProduct = async (data: z.infer<typeof productSchema>) => {
+  const parsed = productSchema.parse(data)
+  await db.insert(products).values(parsed)
+  revalidatePath('/admin')
+  revalidatePath('/products')
+}
+
+export async function deleteProduct(id: string) {
+  await db.delete(products).where(eq(products.id, id))
+  revalidatePath('/admin')
+  revalidatePath('/products')
+}
--- db/queries/products.ts
+++ db/queries/products.ts
@@ -2,8 +2,13 @@ import { eq } from 'drizzle-orm'
 import { db } from '..'
 import { products } from '../schema/products'
 
-export const getProductById = (id: string) =>
-  db.query.products.findFirst({
+export const getProductById = async (id: string) => {
+  return db.query.products.findFirst({
     where: eq(products.id, id),
     with: { book: true }
   })
+}
+
+export const getProducts = async () => {
+  return db.select().from(products).orderBy(products.createdAt)
+}
